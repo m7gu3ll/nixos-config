995132f67661b3c336c367b2601b7acabf90c071
m7gu3ll <m7gu3ll@gmail.com>
2026-06-03 10:23:33 +0100

    26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Miguel's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
   };
 
   outputs =
